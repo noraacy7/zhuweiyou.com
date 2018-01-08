bc58487824173439ce79fd71f9e21c8ad835bf76
zhuweiyou <i@zhuweiyou.com>
2018-01-08 12:15:20 +0800

    更新配置

--- poi.config.js
+++ poi.config.js
@@ -1,17 +1,12 @@
-const OfflinePlugin = require('offline-plugin')
+const offline = require('poi-preset-offline')
 
 module.exports = options => ({
   html: {
-    template: 'src/index.html'
+    template: './src/index.html'
   },
-  webpack (config) {
-    if (options.mode === 'production') {
-      config.plugins.push(new OfflinePlugin({
-        ServiceWorker: {
-          events: true
-        }
-      }))
-    }
-    return config
-  }
+  presets: [
+    offline({
+      pwa: './src/pwa.js'
+    })
+  ]
 })
