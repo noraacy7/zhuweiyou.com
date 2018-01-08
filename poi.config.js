4bbc59b029fcd357e63af197dbcc5cd1946cd48f
zhuweiyou <i@zhuweiyou.com>
2018-01-08 13:00:14 +0800

    移除 pwa

--- poi.config.js
+++ poi.config.js
@@ -1,17 +1,6 @@
-const OfflinePlugin = require('offline-plugin')
-
 module.exports = options => ({
+  entry: './src/index.js',
   html: {
-    template: 'src/index.html'
-  },
-  webpack (config) {
-    if (options.mode === 'production') {
-      config.plugins.push(new OfflinePlugin({
-        ServiceWorker: {
-          events: true
-        }
-      }))
-    }
-    return config
+    template: './src/index.html'
   }
 })
--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import Vue from 'vue'
 import router from './router'
 import App from './component/app'
 
-if (process.env.NODE_ENV === 'production') {
-  require('./util/pwa')
-}
-
 new Vue({
   router,
   el: '#app',
